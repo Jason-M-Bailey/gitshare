3be6b5eb8a181152e153dccb81d1ba30c2acb7f6
Ryan Crowder <crowderyan@gmail.com>
2021-08-01 17:20:48 -0400

    named files to best practice

---
+++ models/Project.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Project extends Model {}
+
+Project.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    madeBy: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rolesNeeded: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    peopleApplied: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "project",
+  }
+);
+
+module.exports = Project;
