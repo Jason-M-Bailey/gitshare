d23fbb626a8c5d049270c634d9d2a8d0efb11432
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-08-12 13:38:41 -0400

    removed update user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -55,29 +55,6 @@ router.post('/logout', (req, res) => {
     } else {
         res.status(404).end();
     }
-})
-
-// Update a user
-router.put('/edit/:id', async (req, res) => {
-    try {
-        const userData = await User.update(
-            {
-                github: req.body.github,
-                linkedin: req.body.linkedin,
-            }, 
-            {
-                where: {
-                    id: req.params.id,
-            },
-        });
-        if (!userData[0]) {
-            res.status(400).json({message: 'No user with that id.'});
-            return;
-        }
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
 });
 
 module.exports = router;
